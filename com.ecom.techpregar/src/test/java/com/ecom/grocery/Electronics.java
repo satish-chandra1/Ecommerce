f5f9fec19c4006562f8b44acab06204457989d0e
satish-chandra1 <satishpathak936@gmail.com>
2022-05-02 12:35:38 +0530

    push to local repository

---
+++ com.ecom.techpregar/src/test/java/com/ecom/grocery/Electronics.java
@@ -0,0 +1,22 @@
+package com.ecom.grocery;
+
+import org.testng.annotations.Test;
+
+public class Electronics {
+
+	@Test
+	public void mobile() {
+		System.out.println("iphone,android"); 
+		
+	}
+	@Test
+	public void headphone() {
+		System.out.println("boat,sony"); 
+		
+	}
+	@Test
+	public void router() {
+		System.out.println("airtel,local"); 
+		
+	}
+}
---
+++ com.ecom.techpregar/src/test/java/com/ecom/grocery/Items.java
@@ -0,0 +1,19 @@
+package com.ecom.grocery;
+
+import org.testng.annotations.Test;
+
+public class Items {
+	@Test
+	public void oil() {
+	System.out.println("oil varieties");	
+	}
+	@Test
+	public void soap() {
+		System.out.println("soap collection");
+	}
+	@Test
+	public void nuts() {
+		System.out.println("nuts collection");
+	}
+
+}
